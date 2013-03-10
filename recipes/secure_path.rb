8037190ca6fced5eae939df83b6592e1a5cf46cc
Nick Muerdter <nick.muerdter@nrel.gov>
2013-03-10 15:27:01 -0600

    Updates for new rbenv cookbook. node.recipe? lookup cleanup.

--- recipes/secure_path.rb
+++ recipes/secure_path.rb
@@ -9,30 +9,22 @@
 
 include_recipe "sudo"
 
-if(node[:recipes].include?("rvm::install") || node.recipe?("rvm::install"))
+if(node.recipe?("rbenv::system"))
   # Add the RVM path, but only if it doesn't already exist. This array uniq
   # approach deals with nodes where this had gotten appended multiple times.
-  path = "/usr/local/rvm/bin:#{node[:authorization][:sudo][:secure_path]}"
+  path = "#{node[:rbenv][:root_path]}/shims:#{node[:rbenv][:root_path]}/bin:/usr/local/bin:#{node[:authorization][:sudo][:secure_path]}"
   path = path.split(":").uniq.join(":")
   node.set[:authorization][:sudo][:secure_path] = path
 end
 
-if(node[:recipes].include?("rbenv") || node.recipe?("rbenv"))
-  # Add the RVM path, but only if it doesn't already exist. This array uniq
-  # approach deals with nodes where this had gotten appended multiple times.
-  path = "#{node[:rbenv][:install_prefix]}/rbenv/shims:#{node[:rbenv][:install_prefix]}/rbenv/bin:#{node[:ruby_build][:prefix]}/bin:#{node[:authorization][:sudo][:secure_path]}"
-  path = path.split(":").uniq.join(":")
-  node.set[:authorization][:sudo][:secure_path] = path
-end
-
-if(node[:recipes].include?("subversion::collabnet_client") || node.recipe?("subversion::collabnet_client"))
+if(node.recipe?("subversion::collabnet_client"))
   # Add the Collabnet path, but only if it doesn't already exist.
   path = "#{node[:subversion][:collabnet][:prefix]}/bin:#{node[:authorization][:sudo][:secure_path]}"
   path = path.split(":").uniq.join(":")
   node.set[:authorization][:sudo][:secure_path] = path
 end
 
-if(node[:recipes].include?("postgresql::server") || node.recipe?("postgresql::server"))
+if(node.recipe?("postgresql::server"))
   if(node[:postgresql][:install_repo] == "pgdg")
     # Add the postgresql path, but only if it doesn't already exist.
     path = "/usr/pgsql-#{node[:postgresql][:version]}/bin:#{node[:authorization][:sudo][:secure_path]}"
